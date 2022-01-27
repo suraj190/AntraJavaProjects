f4c0c16d9a0f923f330578b2bd7e4d044984ee37
Suraj <smel@ucdavis.edu>
2022-01-26 19:39:40 -0800

    Homework2

--- src/main/java/h1.java
+++ src/main/java/h1.java
@@ -1,8 +1,37 @@
+import java.util.*;
+
 public class h1 {
 
     public static void main(String[] args){
 
-        System.out.println("hello");
+        Queue<SNode> maxHeap = new PriorityQueue<>();
+        SNode node1 = new SNode(13);
+        SNode node2 = new SNode(2);
+        SNode node3 = new SNode(140);
 
+        maxHeap.add(node1);
+        maxHeap.add(node2);
+        maxHeap.add(node3);
+
+        System.out.println(maxHeap.stream().findFirst().get().value);
+
+
+    }
+}
+
+class SNode implements Comparable<SNode>{
+
+    int value;
+
+    SNode(int value){
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(SNode node){
+        if (this.value == node.value) {
+            return 0;
+        }
+        return this.value < node.value? 1: -1;
     }
 }
